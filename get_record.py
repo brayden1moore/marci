60f5ddcab035e777e8aabab29bcdb40128abb3d4
unknown <braydenmoore2001@gmail.com>
2023-09-26 19:01:01 -0400

    Update data

--- get_record.py
+++ get_record.py
@@ -23,32 +23,56 @@ for game_id,home,away,season,week,total in tqdm(gbg_and_odds_this_year[['game_id
     if week!=1:
         predictions[game_id] = predict(home,away,season,week,total)
 
-# get record and save it
+# merge data
 predictions_df = pd.DataFrame(predictions).T
 predictions_df['predicted_winner'] = [i['Winner'][0] if type(i['Winner'])==list else None for i in predictions_df[1]]
 predictions_df['predicted_winner'] = predictions_df['predicted_winner'].map(team_abbreviation_to_name)
+predictions_df['predicted_winner_probability'] = [i['Probabilities'][0] if type(i['Probabilities'])==list else None for i in predictions_df[1]]
 predictions_df['predicted_over_under'] = [i['Over/Under'][0] if type(i['Over/Under'])==list else None for i in predictions_df[2]]
-predictions_df = predictions_df.merge(results, left_index=True, right_on='game_id').merge(gbg_and_odds_this_year[['game_id','Total Score Close','home_team','away_team','game_date']]).dropna(subset=['predicted_winner'])
+predictions_df['predicted_over_under_probability'] = [i['Probability'][0] if type(i['Probability'])==list else None for i in predictions_df[2]]
+predictions_df = predictions_df.merge(results, left_index=True, right_on='game_id').merge(gbg_and_odds_this_year[['game_id','Total Score Close','home_team','away_team','game_date','Home Odds Close','Away Odds Close']]).dropna(subset=['predicted_winner'])
 predictions_df['over_under'] = ['Over' if t>tsc else 'Under' if t<tsc else 'Push' for t,tsc in predictions_df[['total','Total Score Close']].values]
 predictions_df['game_date'] = pd.to_datetime(predictions_df['game_date'])
 
-predictions_df['winner_correct'] = (predictions_df['predicted_winner']==predictions_df['winner']).astype(int)
-predictions_df['winner_incorrect'] = (predictions_df['predicted_winner']!=predictions_df['winner']).astype(int)
-predictions_df['over_under_correct'] = (predictions_df['predicted_over_under']==predictions_df['over_under']).astype(int)
-predictions_df['over_under_incorrect'] = (predictions_df['predicted_over_under']!=predictions_df['over_under']).astype(int)
+# get returns
+predictions_df['home'] = predictions_df['home_team'].map(team_abbreviation_to_name)
+predictions_df['away'] = predictions_df['away_team'].map(team_abbreviation_to_name)
+predictions_df['picked_home'] = (predictions_df['home']==predictions_df['predicted_winner'])
+predictions_df['picked_away'] = (predictions_df['away']==predictions_df['predicted_winner'])
 
-winners_correct = predictions_df['winner_correct'].sum()
-winners_incorrect = predictions_df['winner_incorrect'].sum()
-over_unders_correct = predictions_df['over_under_correct'].sum()
-over_unders_incorrect = predictions_df['over_under_incorrect'].sum()
+predictions_df['winner_correct'] = (predictions_df['predicted_winner']==predictions_df['winner'])
+predictions_df['winner_incorrect'] = ((predictions_df['predicted_winner']!=predictions_df['winner']) & (predictions_df['winner']!='Tie'))
+predictions_df['winner_tie'] = (predictions_df['winner']=='Tie')
+predictions_df['over_under_correct'] = (predictions_df['predicted_over_under']==predictions_df['over_under'])
+predictions_df['over_under_incorrect'] = ((predictions_df['predicted_over_under']!=predictions_df['over_under']) & (predictions_df['over_under']!='Push'))
+predictions_df['over_under_push'] = (predictions_df['over_under']=='Push')
+
+predictions_df['winner_return'] = [ao-1 if (pa and wc) else ho-1 if (ph and wc) else -1 for ao,ho,pa,ph,wc in predictions_df[['Away Odds Close','Home Odds Close','picked_away','picked_home','winner_correct']].values]
+predictions_df['over_under_return'] = [0.91 if ouc else -1 for ouc in predictions_df['over_under_correct']]
+
+threshold = 0.6
+
+winners_correct = predictions_df.loc[predictions_df['predicted_winner_probability']>threshold, 'winner_correct'].sum()
+winners_incorrect = predictions_df.loc[predictions_df['predicted_winner_probability']>threshold,'winner_incorrect'].sum()
+winners_tie = predictions_df.loc[predictions_df['predicted_winner_probability']>threshold,'winner_tie'].sum()
+winners_return = predictions_df.loc[predictions_df['predicted_winner_probability']>threshold, 'winner_return'].sum()
+
+over_unders_correct = predictions_df.loc[predictions_df['predicted_over_under_probability']>threshold,'over_under_correct'].sum()
+over_unders_incorrect = predictions_df.loc[predictions_df['predicted_over_under_probability']>threshold,'over_under_incorrect'].sum()
+over_unders_push = predictions_df.loc[predictions_df['predicted_over_under_probability']>threshold,'over_under_push'].sum()
+over_unders_return = predictions_df.loc[predictions_df['predicted_over_under_probability']>threshold,'over_under_return'].sum()
+
 max_date = predictions_df['game_date'].max()
-date_obj = datetime.strptime(max_date, "%m/%d/%Y")
-latest_game = date_obj.strftime("%A, %m/%d")
+latest_game = pd.Timestamp(max_date).strftime("%A, %m/%d")
 
 record = {"winners_correct":str(winners_correct),
         "winners_incorrect":str(winners_incorrect),
+        "winners_tie":("-"+str(winners_tie) if winners_tie>0 else ''),
+        "winners_return":str(round(winners_return,1))+"x return",
         "over_unders_correct":str(over_unders_correct),
         "over_unders_incorrect":str(over_unders_incorrect),
+        "over_unders_push":("-"+str(over_unders_push) if over_unders_push>0 else ''),
+        "over_unders_return":str(round(over_unders_return,1))+"x return",
         "latest_game":latest_game}
 
 import json
--- update_data.bat
+++ update_data.bat
@@ -1,7 +1,10 @@
 python "C:\Users\Brayden\OneDrive - stern.nyu.edu\Brayden Moore LLC\Python\Projects\MARCI 3.0\MARCI-NFL-Betting\Source\Build\update.py"
+python "C:\Users\Brayden\OneDrive - stern.nyu.edu\Brayden Moore LLC\Python\Projects\MARCI 3.0\MARCI-NFL-Betting\get_record.py"
 cd "C:\Users\Brayden\OneDrive - stern.nyu.edu\Brayden Moore LLC\Python\Projects\MARCI 3.0\MARCI-NFL-Betting"
 git add "Source\Data\gbg_and_odds_this_year.csv"
 git add "Source\Data\gbg_this_year.csv"
+git add "Source\Data\results.csv"
+git add "Source\Data\record.json"
 git commit -m "Update with 2023 data"
 git push
 pause
