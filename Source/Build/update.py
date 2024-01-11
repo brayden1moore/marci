74571f6638e20a655d049e29489fa3d808661f42
Brayden Moore <braydenmoore2001@gmail.com>
2024-01-10 19:34:58 -0500

    update update.py

--- Source/Build/update.py
+++ Source/Build/update.py
@@ -1,11 +1,15 @@
+import nfl_data_py.nfl_data_py as nfl
 import build
 import datetime as dt
 import numpy as np
+import io
 import pandas as pd
 pd.set_option('chained_assignment',None)
 pd.set_option('display.max_columns',None)
 import os
 import pickle as pkl
+import requests
+from bs4 import BeautifulSoup
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.dirname(current_directory)
@@ -33,9 +37,69 @@ file_path = os.path.join(pickle_directory, 'schedule.pkl')
 with open(file_path, 'wb') as f:
     pkl.dump(df, f)
 
+def get_week():
+    headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Dnt': '1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    }
+    url = 'https://www.nfl.com/schedules/'
+    resp = requests.get(url,headers=headers)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    h2_tags = soup.find_all('h2')
+    year = h2_tags[0].getText().split(' ')[0]
+    week = h2_tags[0].getText().split(' ')[-1]
+    return int(week), int(year)
+
+def get_lines(season):
+    url = 'https://www.sportsbettingdime.com/nfl/public-betting-trends/'
+    response = requests.get(url)
+    html = BeautifulSoup(response.text)
+    week = html.find_all('h2')[0].get_text().split(' ')[-1]
+    df = pd.read_html(io.StringIO(response.text))
+
+    columns = list(df[0].loc[0,:].values)
+    columns = columns[:2] + columns[3:] 
+
+    data_list = []
+    for data in df[1:-1]:
+        data.columns = columns
+        data['Matchup'] = data['Matchup'].str.extract('([A-Z]+)[^A-Za-z]*$')
+        data_dict = {
+                'season' : season,
+                'week' : week,
+                'home_team' : data['Matchup'][1],
+                'away_team' : data['Matchup'][0],
+                'away_spread' : float(data.iloc[0,4].replace('+','')),
+                'money_on_away_ats' : int(data.iloc[0,5].replace('%',''))/100,
+                'bets_on_away_ats' : int(data.iloc[0,6].replace('%',''))/100,
+                'away_moneyline' : int(data['moneyline'][0].replace('+','')),
+                'money_on_away_ml' : int(data.iloc[0,8].replace('%',''))/100,
+                'bets_on_away_ml' : int(data.iloc[0,9].replace('%',''))/100,
+                'over_under' : data['total'].str.replace('o','').str.replace('u','').astype(float).mean(),
+                'money_on_over' : int(data.iloc[0,11].replace('%',''))/100,
+                'bets_on_over' : int(data.iloc[0,12].replace('%',''))/100
+            }
+        data_list.append(data_dict)
+
+    betting_data = pd.DataFrame(data_list)
+    betting_data['key'] = [f'{season}_{week}_{away}_{home}' for season, week, away, home in betting_data[['season','week','away_team','home_team']].values]
+    return betting_data
+
+current_week = get_week()[0]
+the_week = {'week':current_week,
+            'year':current_season}
+file_path = os.path.join(pickle_directory, 'the_week.pkl')
+with open(file_path, 'wb') as f:
+    pkl.dump(the_week, f)
+
 # update current season
 build.build_gbg_data(get_seasons=[current_season])
-#build.build_gbg_data(get_seasons=range(2014,2024))
 build.add_odds_data()
 
 # get winners
